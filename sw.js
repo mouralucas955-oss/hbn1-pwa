fdf7da7943fd6bc9723ba1cba81992823fe5cc72
mouralucas955-oss <mouralucas955@gmail.com>
2026-07-23 20:20:11 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // SERVICE WORKER — HBN1 PWA v6
 // Cache do app shell com versionamento automático
 // ==========================================================================
-const CACHE_NAME = 'hbn1-shell-v6';
+const CACHE_NAME = 'hbn1-shell-v7';
 const ARQUIVOS_PARA_CACHE = [
   './',
   './index.html',
